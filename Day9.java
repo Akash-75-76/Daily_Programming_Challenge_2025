887b0813b1fcde6aa2d5342d252f1f56b8855b3e
Akash Deshmukh <deshmukhaakash752005@gmail.com>
2025-08-23 16:04:45 +0530

    Learned Longest Common Prefix problem

---
+++ Day9.java
@@ -0,0 +1,33 @@
+public class Day9 {
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        String[] test1 = {"flower", "flow", "flight"};
+        System.out.println(longestCommonPrefix(test1));
+
+        String[] test2 = {"dog", "racecar", "car"};
+        System.out.println(longestCommonPrefix(test2));
+
+        String[] test3 = {"apple", "ape", "april"};
+        System.out.println(longestCommonPrefix(test3));
+
+        String[] test4 = {""};
+        System.out.println(longestCommonPrefix(test4));
+
+        String[] test5 = {"alone"};
+        System.out.println(longestCommonPrefix(test5));
+
+        String[] test6 = {};
+        System.out.println(longestCommonPrefix(test6));
+    }
+}
