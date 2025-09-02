5fba08dc26de5d411c174fec6b5a2632eeeb842b
Akash Deshmukh <deshmukhaakash752005@gmail.com>
2025-09-02 19:20:47 +0530

    Create Day19.java

---
+++ Day19.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+public class Day19{
+    public int evaluatePostfix(String expression) {
+        Stack<Integer> stack = new Stack<>();
+        String[] tokens = expression.split(" ");
+        
+        for (String token : tokens) {
+            if (isOperator(token)) {
+                int b = stack.pop();
+                int a = stack.pop();
+                int result = applyOperation(a, b, token);
+                stack.push(result);
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        
+        return stack.pop();
+    }
+    
+    private boolean isOperator(String token) {
+        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
+    }
+    
+    private int applyOperation(int a, int b, String operator) {
+        switch (operator) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                return a / b; // Integer division truncates towards zero
+            default:
+                throw new IllegalArgumentException("Invalid operator: " + operator);
+        }
+    }
+}
